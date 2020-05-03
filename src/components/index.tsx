8f23b55d065b11690539a99c9765c6d605a2df19
linyimin-bupt <linyimin520812@gmail.com>
2020-05-03 19:04:02 +0800

    add ordinary user page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 import Dashboard from './dashboard/Dashboard'
 import Operator from './user/operator/Operator'
-import OrdinaryUser from './user/view/OrdinaryUser'
+import OrdinaryUser from './user/ordinary/OrdinaryUser'
 
 export default {
     Dashboard,
