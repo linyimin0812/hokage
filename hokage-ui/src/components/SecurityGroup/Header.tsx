b4a5c011e84e26800b252ffb132bf3162cd66f29
linyimin-bupt <linyimin520812@gmail.com>
2020-06-13 23:54:48 +0800

    add header to security group page

---
+++ hokage-ui/src/components/SecurityGroup/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Col, Input, Row } from 'antd';
+
+interface HeaderPropsType {
+    onClick: () => void,
+    onSearch: (address: string) => void
+}
+
+export default class Header extends React.Component<HeaderPropsType> {
+
+    render() {
+        return (
+            <div>
+                <Row
+                    gutter={24}
+                    style={{ backgroundColor: '#e6f7ff', border: '#91d5ff', padding: '4px 0px' }}
+                >
+                    <Col span={12} style={{ display: 'flex', alignItems: 'center' }}>
+                        <Button
+                            style={{ float: 'right' }}
+                            onClick={this.props.onClick}
+                        >
+                            添加安全组
+                        </Button>
+                    </Col>
+                    <Col span={12}>
+                        <Input.Search
+                            placeholder="服务器地址"
+                            onSearch={this.props.onSearch}
+                            style={{ width: '280px', float: "right" }}
+                        />
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+
+}
