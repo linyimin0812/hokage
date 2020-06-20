77ed316d794056e33066e7921a1a23b4f3950d4f
linyimin-bupt <linyimin520812@gmail.com>
2020-06-20 23:30:37 +0800

    add account info

---
+++ hokage-ui/src/components/monitor/serverresource/basicinfo/AccountInfo.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, Table } from 'antd';
+
+const data: any[] = [
+    {
+        type: "system",
+        username: "banzhe",
+        homeDir: "/home/banzhe"
+    },
+    {
+        type: "system",
+        username: "root",
+        homeDir: "/home/root"
+    },
+    {
+        type: "user",
+        username: "linyimin",
+        homeDir: "/home/linyimin"
+    },
+]
+
+export default class AccountInfo extends React.Component<any, any> {
+    render() {
+        return (
+            <Card title="账户信息">
+                <Table dataSource={data} >
+                    <Table.Column title="类型" dataIndex="type" />
+                    <Table.Column title="用户名" dataIndex="username" />
+                    <Table.Column title="工作目录" dataIndex="homeDir" />
+                </Table>
+            </Card>
+        );
+    }
+}
