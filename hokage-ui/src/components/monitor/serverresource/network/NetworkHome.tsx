c3d2c127cadec8fd1b4a2187302351fba11d3a75
linyimin-bupt <linyimin520812@gmail.com>
2020-06-21 01:37:18 +0800

    add network pane home page

---
+++ hokage-ui/src/components/monitor/serverresource/network/NetworkHome.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Col, Divider, Row } from 'antd';
+import InterfaceInfo from './InterfaceInfo';
+import DownloadSpeed from './DownloadSpeed';
+import UploadSpeed from './UploadSpeed';
+import ARPCacheTable from './ARPCacheTable';
+import ConnectionInfo from './ConnectionInfo';
+
+export default class NetworkHome extends React.Component<any, any>{
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col span={8}>
+                        <InterfaceInfo />
+                    </Col>
+                    <Col span={8}>
+                        <ARPCacheTable />
+                    </Col>
+                    <Col span={8}>
+                        <ConnectionInfo />
+                    </Col>
+
+                </Row>
+                <Divider />
+                <Row>
+                    <Col span={10}>
+                        <DownloadSpeed />
+                    </Col>
+                    <Col span={2} />
+                    <Col span={10}>
+                        <UploadSpeed />
+                    </Col>
+                    <Col span={2} />
+                </Row>
+            </div>
+        );
+    }
+}
