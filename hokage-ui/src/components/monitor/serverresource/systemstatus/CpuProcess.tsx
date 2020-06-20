cdd90d30447d2b0e4f38f9f4190899d21d9cb14d
linyimin-bupt <linyimin520812@gmail.com>
2020-06-21 00:00:05 +0800

    add card to Cpu process comoment

--- hokage-ui/src/components/monitor/serverresource/systemstatus/CpuProcess.tsx
+++ hokage-ui/src/components/monitor/serverresource/systemstatus/CpuProcess.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table } from 'antd'
+import { Card, Table } from 'antd';
 
 const data: any[] = [
     {
@@ -24,14 +24,16 @@ export default class CpuProcess extends React.Component<any, any>{
 
     render() {
         return (
-            <Table dataSource={data} pagination={false} scroll={{y: 350}} >
-                <Table.Column title="PID" dataIndex="pid" />
-                <Table.Column title="USER" dataIndex="user" />
-                <Table.Column title="CPU%" dataIndex="cpu" />
-                <Table.Column title="RSS" dataIndex="rss" />
-                <Table.Column title="VSZ" dataIndex="vsz" />
-                <Table.Column title="CMD" dataIndex="cmd" />
-            </Table>
+            <Card title="CPU使用率">
+                <Table dataSource={data} pagination={false} scroll={{y: 350}} >
+                    <Table.Column title="PID" dataIndex="pid" />
+                    <Table.Column title="USER" dataIndex="user" />
+                    <Table.Column title="CPU%" dataIndex="cpu" />
+                    <Table.Column title="RSS" dataIndex="rss" />
+                    <Table.Column title="VSZ" dataIndex="vsz" />
+                    <Table.Column title="CMD" dataIndex="cmd" />
+                </Table>
+            </Card>
         );
     }
 }
