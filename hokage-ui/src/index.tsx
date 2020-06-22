5cd82c95c5af70bbe8e57ded1a2a31e966d006e6
linyimin-bupt <linyimin520812@gmail.com>
2020-06-23 00:50:58 +0800

    remove useless comment

--- hokage-ui/src/index.tsx
+++ hokage-ui/src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Page from './Page';
-// import { AppContainer } from 'react-hot-loader';
 import './style/lib/animate.css';
 import './style/antd/index.less';
 import './style/index.less';
 
 
 ReactDOM.render(
-    // <AppContainer>
         <Page />,
-    // </AppContainer>
     document.getElementById('root')
 );
 serviceWorker.register();
