b55dcd184b3fd2aebbd00d3123896f028da12f08
linyimin-bupt <linyimin520812@gmail.com>
2020-07-27 23:46:31 +0800

    add Hoakge task result dao class

---
+++ src/main/java/com/banzhe/hokage/dao/HokageTaskResultDao.java
@@ -0,0 +1,41 @@
+package com.banzhe.hokage.dao;
+
+import com.banzhe.hokage.dao.dataobject.HokageTaskResultDO;
+
+import java.util.List;
+
+/**
+ * @author linyimin
+ * @date 2020/7/27 11:33 下午
+ * @email linyimin520812@gmail.com
+ * @description
+ */
+public interface HokageTaskResultDao {
+    /**
+     * 插入一条任务执行结果
+     * @param hokageTaskResultDO
+     * @return
+     */
+    Integer insert(HokageTaskResultDO hokageTaskResultDO);
+
+    /**
+     * 更新一条任务执行结果
+     * @param hokageTaskResultDO
+     * @return
+     */
+    Integer update(HokageTaskResultDO hokageTaskResultDO);
+
+    /**
+     * 根据id主键查找任务执行结果
+     * @param id
+     * @return
+     */
+    HokageTaskResultDO findById(Long id);
+
+    /**
+     * 根据任务id查找任务执行结果
+     * @param taskId
+     * @return
+     */
+    List<HokageTaskResultDO> findByTaskId(Long taskId);
+}
