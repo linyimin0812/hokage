d6128d36d3bc0d33009a76c19f9a784472e7a14f
linyimin-bupt <linyimin520812@gmail.com>
2020-11-01 21:23:34 +0800

    add comment in HomeController

--- src/main/java/com/banzhe/hokage/biz/controller/HomeController.java
+++ src/main/java/com/banzhe/hokage/biz/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    /**
+     * 应用入口
+     * @param model
+     * @return
+     */
     @RequestMapping({"/app/**", "/"})
     public String index(Model model) {
         return "index";
