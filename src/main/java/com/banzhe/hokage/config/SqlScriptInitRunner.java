fc90af65575ded03b6aae2341c9daf2cf41c49c8
linyimin-bupt <linyimin520812@gmail.com>
2020-11-19 23:29:09 +0800

    feat: run sql script when project set up

--- src/main/java/com/banzhe/hokage/config/SqlScriptInitRunner.java
+++ src/main/java/com/banzhe/hokage/config/SqlScriptInitRunner.java
@@ -1,7 +1,11 @@
 package com.banzhe.hokage.config;
 
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.jdbc.ScriptRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -10,12 +14,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import javax.sql.DataSource;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 /**
  * @author linyimin
@@ -40,12 +42,14 @@ public class SqlScriptInitRunner implements CommandLineRunner {
         this.dataSource = dataSource;
     }
 
+    private final Logger logger = LoggerFactory.getLogger(SqlScriptInitRunner.class);
+
     @Override
     public void run(String... args) throws Exception {
 
-
-
         ScriptRunner runner = new ScriptRunner(dataSource.getConnection());
+
+        runner.setLogWriter(new PrintWriter(new LogPrintWriter(logger, Level.INFO)));
 
         if (!StringUtils.isBlank(sqlScriptPath)) {
 
@@ -78,8 +82,7 @@ public class SqlScriptInitRunner implements CommandLineRunner {
             // Last run the remaining scripts
             Arrays.stream(Objects.requireNonNull(file.listFiles()))
                     .filter(File::isFile)
-                    .filter(sqlScriptFile -> !StringUtils.contains(sqlScriptFile.getName(), DDL))
-                    .filter(sqlScriptFile -> !StringUtils.contains(sqlScriptFile.getName(), DML))
+                    .filter(sqlScriptFile -> !StringUtils.containsAny(sqlScriptFile.getName(), DDL, DML))
                     .forEach(sqlScriptFile -> {
                         try {
                             runner.runScript(new BufferedReader(new FileReader(sqlScriptFile)));
@@ -87,7 +90,59 @@ public class SqlScriptInitRunner implements CommandLineRunner {
                             // The Exception will never be triggered
                         }
                     });
+        }
+    }
+    @Data
+    private class LogPrintWriter extends Writer {
 
+        private Logger logger;
+        private Level level;
+
+        private StringBuilder mem;
+
+        public LogPrintWriter(Logger logger, Level level) {
+            this.logger = logger;
+            this.level = level;
+            mem = new StringBuilder();
+        }
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            IntStream.range(0, len)
+                    .filter(i -> !StringUtils.equals(String.valueOf(cbuf[i+off]), StringUtils.LF))
+                    .forEach(i -> mem.append(cbuf[i+off]));
+        }
+
+        @Override
+        public void flush() throws IOException {
+            log(mem.toString());
+            mem = new StringBuilder();
+        }
+
+        @Override
+        public void close() throws IOException {}
+
+        private void log(String mem) {
+            switch (level) {
+                case INFO:
+                    logger.info(mem);
+                    break;
+                case TRACE:
+                    logger.trace(mem);
+                    break;
+                case DEBUG:
+                    logger.debug(mem);
+                    break;
+                case WARN:
+                    logger.warn(mem);
+                    break;
+                case ERROR:
+                    logger.error(mem);
+                    break;
+                default:
+                    logger.info(mem);
+                    break;
+            }
         }
     }
 }
