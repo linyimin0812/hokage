709b5074761e4ae1862bf8eae68ced647e451f93
linyimin-bupt <linyimin520812@gmail.com>
2021-02-16 18:54:23 +0800

    docs: add author

--- src/main/java/com/hokage/HokageApplication.java
+++ src/main/java/com/hokage/HokageApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author linyimin
+ * @date 2020/4/21 1:11 am
+ * @email linyimin520812@gmail.com
+ * @description
+ */
+
 @OpenAPIDefinition(
 	info = @Info(
 		title = "Hokage API docs",
