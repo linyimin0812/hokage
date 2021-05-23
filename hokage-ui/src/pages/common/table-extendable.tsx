52f14cbbace70270a79f9113d7cae77f699b2a10
linyimin <lin_yi_min@163.com>
2021-05-23 16:25:18 +0800

    feat: remove useless file and import statement

--- hokage-ui/src/pages/common/table-extendable.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode, ReactText } from 'react'
-
-// 表格嵌套子表配置
-
-export interface TableExtendable {
-	expandedRowKeys: ReactText[],
-	expandedRowRender: () => ReactNode,
-	onExpand: (expanded: boolean, record: any) => void
-}
--- hokage-ui/src/pages/user/operator/index.tsx
+++ hokage-ui/src/pages/user/operator/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import BreadCrumb, { BreadcrumbPrpos } from '../../../layout/bread-crumb'
 import { UserSearch, UserSearchFormType } from '../common/search'
-import { TableExtendable } from '../../common/table-extendable'
-import { UserVO } from '../../../axios/action/user/user-type'
-import { ServerVO } from '../../../axios/action/server/server-type'
 import { Toolbar } from './toolbar'
 import store from './store'
 import { observer } from 'mobx-react'
 import OperatorTable from './table'
 
-type OperatorState = {
-  dataSource: UserVO[],
-  expandable: TableExtendable,
-  nestedTableDataSource: ServerVO[],
-  loading: boolean,
-}
 
 const breadcrumbProps: BreadcrumbPrpos[] = [
   { name: '首页', link: '/app/index' },
@@ -23,7 +14,7 @@ const breadcrumbProps: BreadcrumbPrpos[] = [
 ]
 
 @observer
-export default class Operator extends React.Component<any, OperatorState> {
+export default class Operator extends React.Component {
 
   componentDidMount() {
     store.fetchRecords()
