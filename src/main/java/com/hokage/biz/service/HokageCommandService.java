efb64e6414649ff798beee2f63a35c3647977485
linyimin <lin_yi_min@163.com>
2021-05-28 23:40:41 +0800

    feat: add hokage command service

---
+++ src/main/java/com/hokage/biz/service/HokageCommandService.java
@@ -0,0 +1,10 @@
+package com.hokage.biz.service;
+
+/**
+ * @author yiminlin
+ * @date 2021/05/28 11:37 pm
+ * @description command service interface
+ **/
+public interface HokageCommandService {
+
+}
