8781e3eee018a3f7e72b33fa00d03aea319624fc
linyimin <lin_yi_min@163.com>
2021-06-13 17:56:26 +0800

    fix: support file reader fullscreen

--- hokage-ui/src/pages/file-management/file-reader.tsx
+++ hokage-ui/src/pages/file-management/file-reader.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Modal } from 'antd'
+import { Modal } from 'antd';
 import Editor from 'react-monaco-editor'
 import { FileContentVO } from '../../axios/action/file-management/file-management-type'
+import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons'
 
 export interface FileReaderPropsType {
   visible: boolean,
@@ -9,26 +10,52 @@ export interface FileReaderPropsType {
   close: () => void
 }
 
-export class FileReader extends React.Component<FileReaderPropsType> {
+type FileReaderStateType = {
+  fullScreen: boolean
+}
 
-  renderModalTitle = (contentVO: FileContentVO) => {
-    const { name, curLine, totalLine } = contentVO
-    if (curLine < totalLine) {
-      return <span>{name}<span style={{color: 'red'}}>(此文件过大, 共{totalLine}行, 只显示前{curLine}行)</span></span>
-    } else {
-      return <span>{name}</span>
-    }
+export class FileReader extends React.Component<FileReaderPropsType, FileReaderStateType> {
+
+  state = {
+    fullScreen: false
   }
 
+  toggleFullScreen = () => {
+    const { fullScreen } = this.state
+    this.setState({
+      fullScreen: !fullScreen,
+    })
+  }
+
+  renderModalTitle = (contentVO: FileContentVO) => {
+    const { name } = contentVO
+    const { fullScreen } = this.state
+    return (
+      <>
+        {name}
+        <button type="button" className="ant-modal-close" style={{ right: 42 }} onClick={this.toggleFullScreen}>
+            <span className="ant-modal-close-x">
+              {fullScreen ? <FullscreenExitOutlined translate /> : <FullscreenOutlined translate />}
+            </span>
+        </button>
+      </>
+    )
+  }
 
   render() {
     const { visible, contentVO, close } = this.props
+    const { fullScreen } = this.state
     if (!contentVO.name) {
       return null
     }
     return (
-      <Modal width={'1200px'} title={this.renderModalTitle(contentVO)} visible={visible} footer={null} onCancel={close}>
-        <Editor height={'500px'} options={{readOnly: true}} value={contentVO.content} />
+      <Modal
+        width={fullScreen ? '100vw' : '1200px'}
+        wrapClassName={fullScreen ? 'file-reader-wrapper' : undefined}
+        title={this.renderModalTitle(contentVO)}
+        visible={visible} footer={null} onCancel={close}
+      >
+        <Editor height={visible && fullScreen ? 'calc(100vh - 110px)' : '500px'} options={{readOnly: true}} value={contentVO.content} />
       </Modal>
     )
   }
