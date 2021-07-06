2d1eb804cdf98573c5e7619c39b384c1669b901b
linyimin-bupt <linyimin520812@gmail.com>
2021-07-07 06:40:31 +0800

    fix: docker run error

--- APP-META/docker-config/ubuntu/start-ubuntu.sh
+++ APP-META/docker-config/ubuntu/start-ubuntu.sh
@@ -9,7 +9,7 @@ cd ./20.04 && docker build -t ubuntu-ssh:20.04 . && cd ..
 
 # run
 
-docker run -d -it -p 11114:22 --name ubuntu-ssh:14.04 ubuntu-ssh:14.04
-docker run -d -it -p 11116:22 --name ubuntu-ssh:16.04 ubuntu-ssh:16.04
-docker run -d -it -p 11118:22 --name ubuntu-ssh:18.04 ubuntu-ssh:18.04
-docker run -d -it -p 11120:22 --name ubuntu-ssh:20.04 ubuntu-ssh:20.04
+docker run -d -it -p 11114:22 --name ubuntu-ssh-14.04 ubuntu-ssh:14.04
+docker run -d -it -p 11116:22 --name ubuntu-ssh-16.04 ubuntu-ssh:16.04
+docker run -d -it -p 11118:22 --name ubuntu-ssh-18.04 ubuntu-ssh:18.04
+docker run -d -it -p 11120:22 --name ubuntu-ssh-20.04 ubuntu-ssh:20.04
